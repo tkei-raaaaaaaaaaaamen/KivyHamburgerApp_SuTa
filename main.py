309fe352e53a5ad63eae5d1dc92d225ee8bd5a54
tkei-raaaaaaaaaaaamen <k.takahashi.sys24@morijyobi.ac.jp>
2025-10-06 16:38:35 +0900

    フォントの設定

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# まずは環境構築ですよ、と。
+from kivy.app import App
+from kivy.uix.label import Label
+# フォントの記述
+from kivy.core.text import LabelBase, DEFAULT_FONT
+from kivy.resources import resource_add_path
+import os
+
+# フォントファイルの指定
+font_path = os.path.join(os.path.dirname(__file__), "font", "NotoSansJP-VariableFont_wght.ttf")
+resource_add_path(os.path.dirname(font_path))
+LabelBase.register(DEFAULT_FONT, font_path)
+
+class MyApp(App):
+    def build(self):
+        return Label(text="こんにちは！!")
+
+if __name__ == "__main__":
+    MyApp().run()
